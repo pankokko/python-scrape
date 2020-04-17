9a7bd2204ecec66ec6f4ff8121230a25835ed2b4
pankokko <teshi41@gmail.com>
2020-04-17 19:49:18 +0900

    [update] Apply to infinity scroll

--- scrape.py
+++ scrape.py
@@ -1,5 +1,5 @@
 URL      = "https://itp.ne.jp/keyword/?areaword=&keyword=%E7%97%85%E9%99%A2%E3%83%BB%E5%8C%BB%E9%99%A2"
-# ランキング内の画像がターゲット
+#詳細ページへのリンクを取得
 Selector = ".m-article-card__header__title__link"
 path = "/Users/teshigawararyou/Downloads/chromedriver"
 # 必須
@@ -12,7 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-# Selenium用オプション quiitaの記事をコピーしました。
+# Selenium用オプション quiitaの記事からコピー
 op = Options()
 op.add_argument("--disable-gpu");
 op.add_argument("--disable-extensions");
@@ -28,6 +28,7 @@ WebDriverWait(driver, 30).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, Selector))
 )
 
+#さらに表示ボタンが表示されなくなるまで無限ループ
 while True:
     driver.find_element_by_class_name('m-read-more').click()
     time.sleep(5)
@@ -38,7 +39,7 @@ soup = BeautifulSoup(driver.page_source, features="html.parser")
 for uri in soup.select(Selector):
     link =  uri.get('href')
     URL = link + "about/"
-    # 病院名を取得する
+    # 詳細ページから病院情報を取得する
     Selector1 = "body > div.container > div > div > div > div.main > div > article.item.item-table > div > section.item-body.basic > dl:nth-child(1) > dd"
     Selector2 = "body > div.container > div > div > div > div.main > div > article.item.item-table > div > section.item-body.basic > dl:nth-child(5) > dd > p:nth-child(1)"
     Selector3 = "body > div.container > div > div > div > div.main > div > article.item.item-table > div > section.item-body.basic > dl:nth-child(4) > dd"
